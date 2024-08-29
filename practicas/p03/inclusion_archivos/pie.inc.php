53d4b8ae981d31822c590306147f060e9990d054
EsmeraldaUrb <esmeraldaurbinacinto04@gmail.com>
2024-08-29 07:40:09 -0600

    Archivos encabezado y pie corregidos

--- practicas/p03/inclusion_archivos/pie.inc.php
+++ practicas/p03/inclusion_archivos/pie.inc.php
@@ -5,7 +5,7 @@
             #ffcc99;\"> Final de la página PHP Vínculos útiles : <a href=\"php.net\">php.net</a>
             &nbsp; <a href=\"mysql.org\">mysql.org</a></h1>";
             echo "Nombre del archivo ejecutado: ", $_SERVER['PHP_SELF'],"&nbsp;&nbsp; &nbsp;";
-            echo "Nombre del archivo incluido: ", _FILE_ ,"</div>";
+            echo "Nombre del archivo incluido: ", __FILE__ ,"</div>";
         ?>
     </body>
 </html>
